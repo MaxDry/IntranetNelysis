bbf4dee1de2a9fdd79796e2befb3eb6ab2d375c9
Vigni <lucas.vignijr@gmail.com>
2019-04-21 20:22:17 +0200

    connexion

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Faker;
 
 class UserFixtures extends Fixture 
-{
+{   
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
@@ -19,19 +19,18 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $faker = Faker\Factory::create('fr_FR');
-
+        $this->faker = Faker\Factory::create('fr_FR');
         for ($i = 0; $i < 7; $i++) {
             $user = new User();
-            $user->setFirstName($faker->name);
-            $user->setPseudo($faker->lastName);
-            $user->setAge($faker->numberBetween($min = 10, $max = 40));
-            $user->setEmail($faker->freeEmail);
-            $user->setPassword("azerty");
+            $user->setFirstName($this->faker->name);
+            $user->setPseudo($this->faker->lastName);
+            $user->setAge($this->faker->numberBetween($min = 10, $max = 40));
+            $user->setEmail($this->faker->freeEmail);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, "azerty"));
             $user->setCreatedAt(new \DateTime());
             $user->setUpdatedAt(new \DateTime());
-            $user->setProfile($faker->sentence($nbWords = 2, $variableNbWords = true));
-
+            $user->setProfile($this->faker->sentence($nbWords = 2, $variableNbWords = true));
+            $user->setRoles($this->faker->randomElement($array = array(["ROLE_FONDATEUR"], ["ROLE_ADMIN"], ["ROLE_MODERATEUR"])));
             $manager->persist($user);
         }
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -67,6 +68,56 @@ class User
      * @ORM\OneToMany(targetEntity="App\Entity\News", mappedBy="user")
      */
     private $news;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+    
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
     public function __construct()
     {
